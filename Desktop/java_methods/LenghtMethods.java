c2226136db7a78c5fcc1bcadf9bf05802dcae8cb
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-07-04 19:23:05 +0200

    chore:/ project setup

---
+++ Desktop/java_methods/LenghtMethods.java
@@ -0,0 +1,7 @@
+public class LenghMethods {
+    public static void main(String[] args) {
+        String str = "Hello. World!";
+        int length = str.length();
+        System.out.println("length of the string :" + length);
+    }
+} 
