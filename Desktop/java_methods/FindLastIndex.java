3f74157991bcfae80566d74d9f88323c69744837
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-07-07 11:12:37 +0200

    feat:/lastindex method used successfully

---
+++ Desktop/java_methods/FindLastIndex.java
@@ -0,0 +1,7 @@
+public class FindLastIndex {
+    public static void main(String[] args) {
+        String greeting = "how are you";
+        int index = greeting.lastIndexOf("w");
+        System.out.println(index);
+    }
+}
