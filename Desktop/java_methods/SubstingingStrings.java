93e8c4afa30a1b530e0b32d4f50fdb93d9fa47ba
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-07-07 12:09:38 +0200

    feat:/substring method used successfully

---
+++ Desktop/java_methods/SubstingingStrings.java
@@ -0,0 +1,7 @@
+public class SubstingingStrings {
+    public static void main(String[] args) {
+        String str = "david is good at programming";
+        String subStr = str.substring(1, 22);
+        System.out.println("substring fron index 1 to 12 is :" + subStr);
+    }
+}
