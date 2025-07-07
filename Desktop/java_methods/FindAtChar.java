6fc01e663aac0b7dddb63127dc9c046622e1f562
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-07-07 10:23:29 +0200

    feat:/strings converted into uppercase

---
+++ Desktop/java_methods/FindAtChar.java
@@ -0,0 +1,8 @@
+public class FindAtChar {
+    public static void main(String[] args) {
+        String name = "david";
+        String ch = name.toUpperCase();
+        
+        System.out.println("string changed into uppercase  :" + ch);
+    }
+}
